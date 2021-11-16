558913cc6e1e6e3ea904cd27431b0278c54e39bd
cyberspace1902 <91774682+cyberspace1902@users.noreply.github.com>
2021-11-16 22:10:04 +0800

    Add files via upload

---
+++ offline.bat
@@ -0,0 +1,4 @@
+netsh advfirewall firewall add rule name="GTATRILOGY" dir=out program="C:\Program Files\Rockstar Games\Launcher\Launcher.exe" action=block
+netsh advfirewall firewall add rule name="GTATRILOGY" dir=out program="C:\Program Files\Rockstar Games\Launcher\LauncherPatcher.exe" action=block
+netsh advfirewall firewall add rule name="GTATRILOGY" dir=out program="C:\Program Files\Rockstar Games\Launcher\RockstarService.exe" action=block
+netsh advfirewall firewall add rule name="GTATRILOGY" dir=out program="C:\Program Files\Rockstar Games\Social Club\SocialClubHelper.exe" action=block
